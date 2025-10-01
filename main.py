7e29cd23c11e40c90b2cb87f4ed80b907c71649b
zenfarhan445 <zenfarhan44@gmail.com>
2025-10-01 10:39:03 +0700

    update

---
+++ main.py
@@ -0,0 +1,6 @@
+alas = int(input("Masukkan alas segitiga: "))
+tinggi = int(input("Masukkan tinggi segitiga: "))
+
+luas_segitiga = 0.5 * alas * tinggi
+
+print("Luas segitiga adalah:", luas_segitiga)
